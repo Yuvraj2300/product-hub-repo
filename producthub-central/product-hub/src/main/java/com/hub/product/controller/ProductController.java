4c570ef9a9c218ddfae4f9be742efe5f35b18198
Yuvraj Sharma <yuvraj.sharma@statusneo.com>
2022-06-25 18:10:59 +0530

    Changing project to a multi module mavent project

--- producthub-central/product-hub/src/main/java/com/hub/product/controller/ProductController.java
+++ producthub-central/product-hub/src/main/java/com/hub/product/controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.hub.product.controller;
 
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hub.product.model.ProductRequest;
+import com.hub.product.model.ProductResponse;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,8 +22,8 @@ public class ProductController {
 	 *           of some query params as per their requirements
 	 */
 	@PostMapping(path = "/products", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public void getProduct() {
-
+	public ProductResponse getProduct(@RequestBody ProductRequest request) {
+		return null;
 	}
 
 }
