238893f35b1a31bea44d978c1eeafb042d5fbfbb
Yuvraj Sharma <yuvraj.sharma@statusneo.com>
2022-08-06 14:00:01 +0530

    adding ignore

---
+++ producthub-central/product-hub-config/src/test/java/com/pdt/hub/ProductHubConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdt.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductHubConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
